5a8eb8235eb2a4ef41688645ebccb4b81bb8240e
Artur Teregulov <info@teregulov.com>
2016-11-12 23:11:41 +0300

    nodemon

--- Web/Server/index.js
+++ Web/Server/index.js
@@ -4,7 +4,7 @@ import path from 'path';
 let app = express();
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(3000, () => console.log('Running on port 3000'))
