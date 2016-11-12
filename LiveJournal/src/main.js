9c7a3cc34b9507ecddf4e05ce5d82dea20cef475
naftalmm <naftalmm@gmail.com>
2016-11-12 20:01:39 +0300

    fix promises - add 1s delay

--- LiveJournal/src/main.js
+++ LiveJournal/src/main.js
@@ -1,28 +1,7 @@
 const LiveJournal = require('livejournal');
 const jsonfile = require('jsonfile');
 
-function getIdsString(from, to) {
-    var res = '';
-    for (var i = from; i < to; i++) {
-        res += i + ','
-    }
-    return res + to;
-}
-
-function getPosts(journalName, fromId, toId) {
-    return new Promise((resolve, reject) => {
-        //для selecttype: 'multiple' допускается получение не более 99 постов за раз
-        LiveJournal.xmlrpc.getevents({
-            journal: journalName,
-            auth_method: 'noauth',
-            selecttype: 'multiple',
-            itemids: getIdsString(fromId, toId)
-        }, function (err, value) {
-            console.log(fromId, toId, err);
-            setTimeout(()=>resolve(value.events), 1000);
-        });
-    });
-}
+var posts = [];
 
 function getLatestId(journalName) {
     return new Promise((resolve, reject) => {
@@ -37,34 +16,66 @@ function getLatestId(journalName) {
     });
 }
 
-function getAllUserPosts(journalName) {
-    var promises = [];
-    getLatestId(journalName).then((maxId) => {
-        for (var i = 0; i <= maxId / 100; i++) {
-            // for (var i = 0; i <= 3; i++) {
-            promises.push(getPosts(journalName, i * 100 + 1, Math.min(i * 100 + 99, maxId)));
-        }
-        Promise.all(promises).then((promiseResults)=> {
-            var posts = [];
-            promiseResults.forEach((events) => {
-                events.forEach(event => {
-                    posts.push({
-                        id: event.itemid,
-                        datetime: event.eventtime,
-                        url: event.url,
-                        subject: event.subject,
-                        post: event.event,
-                        tags: event.props.taglist
-                    });
-                });
-            });
-            jsonfile.writeFile(`./../result/${journalName}.json`, {posts: posts}, (err) => {
-                if (err != null)
-                    console.error(err)
-            });
+function getPosts(journalName, fromId, toId) {
+    return new Promise((resolve, reject) => {
+        //для selecttype: 'multiple' допускается получение не более 99 постов за раз
+        LiveJournal.xmlrpc.getevents({
+            journal: journalName,
+            auth_method: 'noauth',
+            selecttype: 'multiple',
+            itemids: getIdsString(fromId, toId)
+        }, function (err, value) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(value.events);
+            }
         });
     });
 }
 
-// getPosts('tema', 1, 15).then(console.log);
+function getIdsString(from, to) {
+    var res = '';
+    for (var i = from; i < to; i++) {
+        res += i + ','
+    }
+    return res + to;
+}
+
+function getAllUserPosts(journalName) {
+    getLatestId(journalName).then((maxId) => {
+        let i = 0;
+        let intervalId = setInterval(()=> {
+            if (i <= maxId / 100) {
+                getPosts(journalName, i * 100 + 1, Math.min(i * 100 + 99, maxId)).then(parsePosts, console.log);
+                i++;
+            } else {
+                writeToFile(journalName);
+                clearInterval(intervalId);
+            }
+        }, 1000);
+    });
+}
+
+function parsePosts(events) {
+    events.forEach(event => {
+        posts.push({
+            id: event.itemid,
+            datetime: event.eventtime,
+            url: event.url,
+            subject: event.subject,
+            post: event.event,
+            tags: event.props.taglist
+        });
+    });
+}
+
+function writeToFile(journalName) {
+    console.log(`Скачано ${posts.length} постов`);
+    jsonfile.writeFile(`./../result/${journalName}.json`, {posts: posts}, (err) => {
+        if (err != null)
+            console.error(err)
+    });
+}
+
 getAllUserPosts('tema');
